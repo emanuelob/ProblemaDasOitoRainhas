b2349ace867a60337d2e0f5e357722a127ebfb8e
emanuelob <oliveirabar.emanuel@gmail.com>
2024-05-19 20:10:30 -0300

    :test_tube: teste 11: tabuleiro com solução inviável

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -28,7 +28,7 @@ TEST_CASE("Verifica tabuleiros que são uma solução para o problema", "[tabule
     REQUIRE(tabuleiro10.verifica() == 1);
 }
 
-// TEST_CASE("Verifica tabuleiro que não são uma solução para o problema", "[tabuleiros]") {
-//     Rainhas tabuleiro("teste_8_rainhas.txt"); 
-//     REQUIRE(tabuleiro.verifica() == 0);
-// }
+TEST_CASE("Verifica tabuleiros que não são uma solução para o problema", "[tabuleiros]") {
+    Rainhas tabuleiro11("tabuleiros/teste11.txt"); 
+    REQUIRE(tabuleiro11.verifica() == 0);
+}
